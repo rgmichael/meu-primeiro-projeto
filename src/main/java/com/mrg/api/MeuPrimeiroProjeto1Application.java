4de064c988c679bcc08b9f9643e4dd51826160ce
rgmichael <rgmichael@gmail.com>
2018-09-05 22:38:10 -0300

    teste

---
+++ src/main/java/com/mrg/api/MeuPrimeiroProjeto1Application.java
@@ -0,0 +1,12 @@
+package com.mrg.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuPrimeiroProjeto1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuPrimeiroProjeto1Application.class, args);
+	}
+}
