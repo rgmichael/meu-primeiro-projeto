e263e3e17422d2cc08e7ff9ec815c147eb0cd75f
rgmichael <rgmichael@gmail.com>
2018-09-05 22:43:20 -0300

    teste alteração

--- src/main/java/com/mrg/api/MeuPrimeiroProjeto1Application.java
+++ src/main/java/com/mrg/api/MeuPrimeiroProjeto1Application.java
@@ -8,5 +8,6 @@ public class MeuPrimeiroProjeto1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MeuPrimeiroProjeto1Application.class, args);
+		System.out.println("teste");
 	}
 }
